b9c9550505c6cfba9e1bf2c68e1175ee8c4f2f2e
Your Name <youremail@gmail.com>
2025-12-25 01:00:18 -0800

    Area of a triangle

---
+++ area of triangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float w,h,a;
+	cout<<"enter the width\n";
+	cin>>w;
+	cout<<"enter the height\n";
+	cin>>h;
+	a=float(0.5)*(w*h);
+	cout<<"Area of triangle "<<a;
+	
+	
+	
+}
